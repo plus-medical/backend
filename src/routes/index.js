926b7542e6aee766ca0b4eb297b27a8e506b7703
Edwin García <spark.com.co@gmail.com>
2020-05-31 14:58:52 -0500

    [feature] build and retrieve pdf

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const usersRouter = require('./users');
 const authRouter = require('./auth');
 const AWSUploadsRouter = require('./awsUploads');
 const readFileRouter = require('./readFile');
+const generatePDFRouter = require('./generatePDF');
 
 const routes = (app) => {
   const router = express.Router();
@@ -10,6 +11,7 @@ const routes = (app) => {
   app.use('/users', usersRouter);
   app.use('/upload', AWSUploadsRouter);
   app.use('/read-file', readFileRouter);
+  app.use('/pdf', generatePDFRouter);
 
   app.use('/', router);
   router.get('/', async (req, res) => {
